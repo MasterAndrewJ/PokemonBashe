81305c20eaf8f62c8b31a1b75f20064979588c91
MasterAndrewJ <masterandrewjamesbryan@gmail.com>
2017-01-25 13:00:27 -0600

    Add files via upload

--- Pokemon_Game/src/AI_Battle.java
+++ Pokemon_Game/src/AI_Battle.java
@@ -1,33 +1,46 @@
-package com.mkyong.csv;
 
-import java.io.BufferedReader;
+
+import java.io.BufferedReader; 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-
+import java.util.Scanner;
 public class AI_Battle {
-	Main poke = new Main();
-int poke_op=poke.poke_op;
-int poke_1=poke.poke_1;	
-Set,Integer. mySet= new HasSet<Integer>();
-String csvfile = "C:\Users\250148\workspace\Pokemon_Game\resource\Pokemon.csv";
-BufferedReader br= null;
-String line ="";
-String SplitBy=",";
-Br= new BufferedReader(new FileReader(csvfile));
-for(int i=1;i>0; i++){
-while((line=br.readLine())) != null){
-	String[] poke= line.split(SplitBy); 
-	String[]poke.myset.add(i);
-}
-}
-System.out.println("What will you do?");	
+	static final int pokemonNUMBER = 365;
+	public AI_Battle() throws Exception{
+		Main poke = new Main();
+		int poke_op=poke.poke_op;
+		int poke_1=poke.poke_1;	
+		//HashSet<Integer> mySet= new HashSet<Integer>();
+		String csvfile = "C:\\Users\\250148\\workspace\\Pokemon_Game\\resource\\Pokemon.csv";
+		BufferedReader br= null;
+		String line ="";
+		String splitBy= ",";
+		br = new BufferedReader(new FileReader(csvfile));
+		String[][] pokeLIST = new String[pokemonNUMBER][] ;
+		for(int i=0;(line=br.readLine()) != null && i < pokemonNUMBER;i++){
+			pokeLIST[i] = line.split(splitBy);
+		}
+		for(int j = 0; j < pokemonNUMBER; j++)
+		{
+			for(int i = 0; i < pokeLIST[j].length; i++)
+				System.out.print(pokeLIST[j][i] + " ");
+		System.out.println("");
+		}
+		System.out.println("What will you do?");	
+	}
 	
-	
-	
-public static void Battle{
+	public static void main(String [] args0){
+		try {
+			AI_Battle t = new AI_Battle();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+public void Battle(){
 	
 	
 }
