466fab5613d48efe026b8e276a88b036a0c53819
MasterAndrewJ <masterandrewjamesbryan@gmail.com>
2017-03-30 12:46:50 -0500

    Add files via upload

---
+++ T/NewTest/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+
+import java.awt.*;
+import java.awt.Graphics;
+import java.util.Scanner;
+
+import javax.swing.*;
+public class Main {
+	int poke_op=20;
+	int poke_1=94;
+	public static void main(String[] args)throws Exception{
+		Battle b = new Battle();
+		Picture p =new Picture();
+		p.UserScreen();
+		Scanner Keyboard = new Scanner(System.in);
+		System.out.println("enter an integer");
+		int myint = Keyboard.nextInt();
+		System.out.println(myint+"j");
+	}
+
+}
